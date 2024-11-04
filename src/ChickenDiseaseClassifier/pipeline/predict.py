24ff1aefe906f55f93bfbe0427b2eb27bb55ec09
Mateen Khan <mateenmk2428@gmail.com>
2024-11-04 15:29:16 +0530

    Webapp and prediction added

---
+++ src/ChickenDiseaseClassifier/pipeline/predict.py
@@ -0,0 +1,31 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+
+
+class PredictionPipeline:
+    def __init__(self, filename):
+        self.filename = filename
+
+
+    def predict(self):
+        # load_model
+        model = load_model(os.path.join("artifacts","training","model.keras"))
+
+        image_name = self.filename
+        test_image = image.load_img(image_name, target_size=(224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+
+        result = np.argmax(model.predict(test_image), axis=1)
+
+        print(result)
+
+        if result[0] == 1:
+            prediction = "Healthy Chicken"
+            return [{"image": prediction}]
+        else:
+            prediction = "Coccidiosis Chicken"
+            return [{"image": prediction}]
